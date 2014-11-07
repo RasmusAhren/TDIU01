1eac9868119176102a7e78c15169c00396f75ae6
RasmusAhren <rasmusahren@hotmail.com>
2014-11-07 10:08:03 +0100

    Update lab_03.cc

--- lab_03.cc
+++ lab_03.cc
@@ -7,6 +7,7 @@ struct List_Type
   int data;
   List next;
 };
+void insert_first(List & orglist, const int val); // TEMPORÄR LISTBYGGARE "PLAGIAT!"
 
 bool insert(List & orglist,const int val);
 bool remove(List & orglist, const int val);
@@ -19,52 +20,103 @@ void clear(List & orglist);
 int size(const List orglist);
 
 
+
+
+/*---------------------------------------------------------*/
 int main()
 {
+  List test{new List_Type{0, nullptr}};
+  for(int i{1}; i<15; i+=3)
+    {
+    insert_first(test, i);
+    }
+
+  remove(test,7);
 
 
-  List test{new List_Type{15, nullptr}};
-  empty(test);
-  member(test,12);
-  /*  cout << test->next << endl;
-  delete test;
 
-  int* testa{new int{16}};
-  cout << *testa;
-  delete testa;
-*/
-  delete test;
+  //  delete test;
   return 0;
 }
+/*---------------------------------------------------------*/
+
+
+
+
 
 
 bool empty(const List orglist)
 {
   if(orglist == nullptr)
     {
-      //      cout << "hazza!" << endl;
+      cout << "empty() = TRUE " << endl;
       return true;
     }
   else
     {
-      //      cout << "aaww" << endl;
+      cout << "Data i empty(): " << orglist->data << endl;
+      cout << "empty() = FALSE" << endl;
       return false;
     }
 }
 
 bool member(const List orglist, const int val)
 {
-  if(empty(orglist) || orglist->data > val)
+  cout << "chekcing: " << val << endl;
+  if(empty(orglist))// || orglist->data > val)
     {
-      cout << "false!" << endl;
+      cout << orglist->data << endl;
+      cout << "member() = false!" << endl;
       return false;
     }
   else if(orglist->data == val)
     {
-      cout << "true" << endl; 
+      cout << "member() = true" << endl; 
       return true;
     }
   else
     cout << "omstart!" << endl;
     return (member(orglist->next,val));
+}
+
+bool remove(List & orglist, const int val)
+{
+  if(empty(orglist))
+    {
+      cout << "remove() = FALSE!" << endl;
+      return false;
+    }
+  else if(orglist->data == val)
+    {
+      cout << "remove() = TRUE!" << endl;
+
+      List temp{orglist}
+
+      delete orglist;
+
+      return true;
+    }
+}
+
+void insert_first(List & orglist, const int val) //PLAGIAT TEST
+{
+  cout << "Insert First med Val = " << val << endl;
+  if(empty(orglist))
+    {
+      List temp{new List_Type};
+      temp->data = val;
+      temp->next = nullptr;
+      orglist = temp;
+    }
+  else
+    {
+
+      cout << "insert.org: " << orglist << endl;
+
+      List temp{new List_Type};
+      temp->data = val;
+      temp->next = orglist;
+      orglist = temp;
+      cout << "insert.temp: " << temp << endl;
+    }
 }
