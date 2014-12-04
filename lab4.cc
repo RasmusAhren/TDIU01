d01a2b79b60fbfef7eae6d756c9d267b6aaa9ee9
RasmusAhren <rasmusahren@hotmail.com>
2014-12-04 12:22:53 +0100

    Update lab4.cc

--- lab4.cc
+++ lab4.cc
@@ -13,10 +13,16 @@ struct deltagare
   string surname{}; 
   string club{};
   string result{};
+  List next{};
 };
 
+
+void print(const List orglist);
+void clear(List& lista);
 void print(vector<deltagare> const & list);
-void inmatning(const string& personfil, List& deltagar_lista);
+bool inmatning(ifstream& personfil, List& deltagar_lista);
+//bool inmatning(ifstream& personfil, List& deltagar_lista);
+bool rekmatning(ifstream& personfil, List& deltagar_lista);
 
 int main(int argc, char* argv[])
 {
@@ -26,20 +32,35 @@ int main(int argc, char* argv[])
 	   << endl;
       return 1;
     }
-  string personfil{argv[1]};
+  string personfilnamn{argv[1]};
   cerr << argc << endl;
-  cerr << personfil << endl;
+  cerr << personfilnamn << endl;
   List lista{};
+  ifstream ifs{personfilnamn};
 
 
-  inmatning(personfil,lista);
+  inmatning(ifs,lista);
+  ifs.close();
 
+  print(lista);
+
+  clear(lista);
 
   return 0;
 }
 
+
+/*-------
+
 void inmatning(const string& personfil, List& deltagar_lista)
 {
+  if(deltagar_lista != nullptr)
+    {
+      cerr << "inmatning: Deltagarlista ej tom" << endl;
+      return;
+    }
+
+  deltagare temp{};
   
   cerr << " --- inmatning startar: " << endl;
 
@@ -47,28 +68,80 @@ void inmatning(const string& personfil, List& deltagar_lista)
   if (!ifs)
     {
       cerr << "Fel i inmatning() " << endl;
+      return;
     }
 
-  // skräp forsats bara för .. jag vet inte.
-  for(int tic{};tic < 1; ++tic)
+
+  cin.ignore(9001,'/n');
+    
+  while(!ifs.eof())
     {
-      cout << ifs << endl;
-    }
+      ifs >> temp->start >> temp->name >> temp->surname;
+      get.line(ifs, temp->club);
 
+      List newtemp{new deltagare};
+      temp->next = newtemp;
+    } 
 
   ifs.close();
   return;
 }
 
+-------*/
 
-
-void print(vector<deltagare> const & list)
+bool inmatning(ifstream& personfil, List& deltagar_lista)
 {
-  for(int i{}; i < list.size(); ++i)
+  if(personfil.eof())
     {
-      cout << endl << list.at(i).start << ' ' << list.at(i).name << ' ' << list.at(i).surname << ' ' << list.at(i).club;
+      return false;
     }
 
-  cout << endl;
+  personfil.ignore(9001,'\n');
 
+  return rekmatning(personfil,deltagar_lista);
+}
+
+bool rekmatning(ifstream& personfil, List& deltagar_lista)
+{
+  //  cerr << "Rekmatning start: " << endl;
+  if(personfil.eof())
+    {
+      return true;
+    }
+
+    List temp(new deltagare);
+    personfil >> temp->start >> temp->name >> temp->surname;
+
+    getline(personfil, temp->club);
+    
+    temp->next = deltagar_lista;
+    deltagar_lista = temp;
+
+    return (rekmatning(personfil, temp->next));
+}
+
+void print(const List orglist)
+{
+  if(orglist == nullptr)
+    {
+      return;
+    }
+  else
+    {
+      cout << orglist->name << ", " << flush << endl;
+      print(orglist->next);
+    }
+}
+
+
+void clear(List &lista)
+{
+  if(lista == nullptr)
+    return;
+  else if(!(lista->next == nullptr))
+    {
+      clear(lista->next);
+    }
+  delete lista;
+  lista =  nullptr;
 }
