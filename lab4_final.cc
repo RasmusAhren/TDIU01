8762c1f13ce9868f599716fcc2f03d83c5bf9793
RasmusAhren <rasmusahren@hotmail.com>
2014-12-16 14:34:06 +0100

    Update lab4_final.cc

--- lab4_final.cc
+++ lab4_final.cc
@@ -2,22 +2,28 @@
 #include <iomanip>
 #include <vector>
 #include <fstream>
+#include <sstream>
 using namespace std;
 using List = struct deltagare *;
-
-
 struct deltagare
 {
-  int start{}; 
-  string name{}; 
-  string surname{}; 
+  int start{};
+  string name{};
+  string surname{};
   string club{};
   double result{};
   List next{};
 };
 
+bool operator>(const deltagare& currentlist ,const deltagare& next)
+{
+  return currentlist.result > next.result
+    || (currentlist.result == next.result && currentlist.name > next.name)
+    || (currentlist.result == next.result && currentlist.name == next.name && currentlist.surname > next.surname);
+}
 
-bool member(const List orglist, const int val, const double result);
+
+bool insert_result(const List orglist, const int val, const double result);
 bool empty(const List orglist);
 void print(const List orglist, ostream & f=std::cout, int ticker=1);
 void clear(List& lista);
@@ -31,7 +37,7 @@ int main(int argc, char* argv[])
 {
   if(argc !=4)
     {
-            cerr << "fel antal filer som kommandoradsargument. (Det ska vara 3 filer)"
+      cerr << "fel antal filer som kommandoradsargument. (Det ska vara 3 filer)"
 	   << endl;
       return 1;
     }
@@ -48,36 +54,33 @@ int main(int argc, char* argv[])
     {
       cerr << "Fel vid öppning av " << argv[3] << endl;
     }
-
   inmatning(ifs,lista);
   ifs.close();
-
   ifs.open(argv[2]);
+  if(!ifs)
+    {
+      cerr << "Fel vid öppning av " << argv[2] << endl;
+    }
   tid_inmatning(ifs,lista);
 
   ofs << left << setw(7) << "Plats" << setw(11) << "Sluttid" << setw(15) << "Förnamn" << setw(14) << "Efternamn" << setw(15) << "Klubb" << endl;
-  //  print(lista,cout);
+
+  // print(lista,cout);
   result_sort(lista,lista);
   print(lista,ofs);
   ofs.close();
   clear(lista);
-
   return 0;
 }
-
-
 bool inmatning(ifstream& personfil, List& deltagar_lista)
 {
   if(personfil.eof())
     {
       return false;
     }
-
   personfil.ignore(9001,'\n');
-
   return rekmatning(personfil,deltagar_lista);
 }
-
 bool rekmatning(ifstream& personfil, List& deltagar_lista)
 {
   personfil.peek();
@@ -85,32 +88,21 @@ bool rekmatning(ifstream& personfil, List& deltagar_lista)
     {
       return true;
     }
-
   List temp(new deltagare);
-  personfil >> temp->start >> temp->name >> temp->surname;
-
+  personfil >> temp->start >> temp->name >> temp->surname >> ws;
   getline(personfil, temp->club);
-    
   temp->next = deltagar_lista;
   deltagar_lista = temp;
-
   return (rekmatning(personfil, temp->next));
 }
-
 void print(const List orglist, ostream & f, int ticker)
 {
-  if(orglist == nullptr)
+  if(orglist != nullptr)
     {
-      return;
-    }
-  else
-    {
-      f << left << setw(7) << ticker << setw(11) << orglist->result << setw(14) << orglist->name << setw(-1) << orglist->surname << setw(14) << orglist->club << endl;
+      f << left << setw(7) << ticker << setw(11) << orglist->result << setw(15) << orglist->name << setw(14) << orglist->surname << setw(14) << orglist->club << endl;
       print(orglist->next,f,(ticker+1));
     }
 }
-
-
 void clear(List &lista)
 {
   if(lista == nullptr)
@@ -120,9 +112,8 @@ void clear(List &lista)
       clear(lista->next);
     }
   delete lista;
-  lista =  nullptr;
+  lista = nullptr;
 }
-
 bool empty(const List orglist)
 {
   if(orglist == nullptr)
@@ -134,8 +125,7 @@ bool empty(const List orglist)
       return false;
     }
 }
-
-bool member(const List orglist, const int start, const double result)
+bool insert_result(const List orglist, const int start, const double result)
 {
   if(empty(orglist))
     {
@@ -144,30 +134,34 @@ bool member(const List orglist, const int start, const double result)
   else if(orglist->start == start)
     {
       orglist->result = result;
-       return true;
+      return true;
     }
   else
-    return (member(orglist->next,start,result));
+    return (insert_result(orglist->next,start,result));
 }
+
 
 bool tid_inmatning(ifstream & resultfil, List& orglist)
 {
   int tempStart{};
   double tempResult{};
-
-
+  string str{};
   resultfil.ignore(9001,'\n');
+  
 
-  while(resultfil >> tempStart)
+  while(getline(resultfil,str) )
     {
-      while(resultfil.peek() != '\n')
+      stringstream ss{str};
+      ss >> tempStart;
+      while(ss >> tempResult)
 	{
-	  resultfil >> tempResult;
+	  
 	}
-  member(orglist, tempStart, tempResult);
+      insert_result(orglist, tempStart, tempResult);
     }
   return true;
 }
+
 
 
 bool result_sort(List& orglist, List& currentlist)
@@ -176,25 +170,10 @@ bool result_sort(List& orglist, List& currentlist)
     return false;
   else if(empty(currentlist->next))
     return true;
-
-  if(currentlist->result > currentlist->next->result)
+  cerr << "i result-sort" << endl;
+  if ( (*currentlist) > (*currentlist->next))
     {
-      List temp{currentlist->next};
-      currentlist->next = currentlist->next->next;
-      temp->next = currentlist;
-      currentlist = temp;
-      return result_sort(orglist, orglist);
-    }
-  else if(currentlist->result == currentlist->next->result && currentlist->name > currentlist->next->name)
-    {
-      List temp{currentlist->next};
-      currentlist->next = currentlist->next->next;
-      temp->next = currentlist;
-      currentlist = temp;
-      return result_sort(orglist, orglist);
-    }
-  else if(currentlist->result == currentlist->next->result && currentlist->name == currentlist->next->name && currentlist->surname > currentlist->next->surname)
-    {
+      cerr << "i if sats" << endl;
       List temp{currentlist->next};
       currentlist->next = currentlist->next->next;
       temp->next = currentlist;
@@ -205,6 +184,4 @@ bool result_sort(List& orglist, List& currentlist)
     {
       return result_sort(orglist, currentlist->next);
     }
-
-
 }
